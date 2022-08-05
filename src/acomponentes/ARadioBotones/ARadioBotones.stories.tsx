032fd165785be20846c0b873197bd4ce722ffdea
Allam05 <xyzalan3030@gmail.com>
2022-08-05 16:56:16 -0600

    Historia de ARadioBoton

---
+++ src/acomponentes/ARadioBotones/ARadioBotones.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+    ComponentStory,
+    ComponentMeta,
+} from '@storybook/react';
+
+import ARadioBotones from './index';
+
+export default {
+    title: "react-sgacomponentes/ARadioBotones",
+    component: ARadioBotones
+} as ComponentMeta<typeof ARadioBotones>;
+
+const ARadioBotonesPlantilla: ComponentStory<typeof ARadioBotones> = (args) => <ARadioBotones {...args} />;
+
+export const ARadioBotonesVisualizacion = ARadioBotonesPlantilla.bind({});
+
+ARadioBotonesVisualizacion.args = {
+    titulo: "Titulo del ARadioBotones",
+    opciones: [
+        { texto: "Opcion 1", valor: "1" },
+        { texto: "Opcion 2", valor: "2" },
+        { texto: "Opcion 3", valor: "3" }
+    ],
+    cambioSeleccion: (valor) => console.log(valor)
+}
